8a5c73a0dd7cc2bb0b8cac15458fbb474b55c466
iliangogov <iliangogov@gmail.com>
2016-10-26 20:49:46 +0300

    CreationalDesignPatterns.sln patterns / SingletonExample.csproj added

---
+++ Topics/03. Creational-Design-Patterns/homework/SingletonExample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SingletonExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            string textExample = "Singleton example works!";
+            var writer = SpecificWriter.Instance;
+
+            writer.WriteTextToConsole(textExample);
+        }
+    }
+}
---
+++ Topics/03. Creational-Design-Patterns/homework/SingletonExample/SpecificWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    public class SpecificWriter
+    {
+        private static SpecificWriter instance;
+
+        internal SpecificWriter()
+        {
+        }
+
+        public static SpecificWriter Instance
+        {
+            get
+            {
+                if (SpecificWriter.instance == null)
+                {
+                    SpecificWriter.instance = new SpecificWriter();
+                }
+
+                return SpecificWriter.instance;
+            }
+        }
+
+        public void WriteTextToConsole(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
