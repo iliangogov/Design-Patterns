f61d41a60ced423581299c5cc9db595c4d5c55c6
iliangogov <iliangogov@gmail.com>
2016-10-26 23:01:03 +0300

    HW done

---
+++ Topics/03. Creational-Design-Patterns/homework/FluentInterfaceExample/Cat.cs
@@ -0,0 +1,54 @@
+﻿namespace FluentInterfaceExample
+{
+    public class Cat
+    {
+        public string Name { get; private set; }
+
+        public string FurColor { get; private set; }
+
+        public string State { get; private set; }
+
+        public int AgeInMonths { get; private set; }
+
+        public Cat()
+        {
+        }
+
+        public Cat(string name, string color, string state, int ageInMonths)
+        {
+            this.Name = name;
+            this.FurColor = color;
+            this.State = state;
+            this.AgeInMonths = ageInMonths;
+        }
+
+        public Cat WithName(string name)
+        {
+            this.Name = name;
+            return this;
+        }
+
+        public Cat WithColor(string color)
+        {
+            this.FurColor = color;
+            return this;
+        }
+
+        public Cat WithState(string state)
+        {
+            this.State = state;
+            return this;
+        }
+
+        public Cat WithAge(int age)
+        {
+            this.AgeInMonths = age;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return "The " + this.FurColor + " cat " + this.Name + " is " + this.AgeInMonths + " months old and is " + this.State;
+        }
+    }
+}
---
+++ Topics/03. Creational-Design-Patterns/homework/FluentInterfaceExample/Run.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FluentInterfaceExample
+{
+    class Run
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cats: ");
+
+            var catOne = new Cat("Maca", "black", "purring", 2);
+            var catTwo = new Cat()
+                              .WithName("Masha")
+                              .WithAge(2)
+                              .WithColor("black")
+                              .WithState("purring");
+
+            Console.WriteLine("Cat 1: " + catOne);
+            Console.WriteLine("Cat 2: " + catTwo);
+
+            // fluent switch
+
+            Console.WriteLine("\nSwitch demo: ");
+
+            // switch arg
+            var person = "Pesho";
+
+            // enable/disable fallthrough
+            var fallThrough = true;
+
+            new Switch<string>(person, fallThrough)
+                .Case("Pesho", () => Console.WriteLine("Pesho is used for examples"))
+                .Case(name => name == "Ivan", () => Console.WriteLine("Ivan is dude"))
+                .Case(person.Length == 5, () => Console.WriteLine("I like names with 5 symbols"))
+                .Default(() => Console.WriteLine("Thats enough ,fallThrough is over"));
+        }
+    }
+}
